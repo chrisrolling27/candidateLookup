e98034bf919cefab1bd8fdda92d8d55416640de1
chris <chrisrolling27@gmail.com>
2024-01-20 11:56:12 -0800

    improved content.js

--- content.js
+++ content.js
@@ -1,19 +1,10 @@
-async function sendHighlightedText() {
-  const highlightedText = "tryversion1";
-  //=window.getSelection().toString();
+console.log("content script here");
 
-  // Send the highlighted text to the background script
-  const response = await chrome.runtime.sendMessage({ text: highlightedText });
-  console.log(response);
-}
+document.addEventListener("mouseup", async function (event) {
+  var selection = window.getSelection();
+  var selectionText = selection.toString().trim();
 
-sendHighlightedText();
-
-//listens for mouseup highlighted text
-document.addEventListener("mouseup", () => {
-  const selectedText = "tryversion2";
-  //=window.getSelection().toString();
-  if (selectedText) {
-    // Handle the highlighted text
+  if (selectionText && selectionText !== "") {
+    const response = await chrome.runtime.sendMessage({ text: selectionText });
   }
 });
